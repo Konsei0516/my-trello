002cf6546d169caf7f600115a08480332acd9c8b
Konsei0516 <masaya19960516@gmail.com>
2020-04-30 20:46:20 +0900

    component設定
    store以外の設定

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import store from './store'
 import './assets/css/styles.css'
 
 Vue.config.productionTip = false
---
+++ src/store/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const savedLists = localStorage.getItem('trello-lists')
+
+const store = new Vuex.Store({
+　state: {
+　  lists: savedLists ? JSON.parse(savedLists): [
+      {
+        title: 'Backlog',
+        cards: [
+          { body: 'English' },
+          { body: 'Mathematics' },
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [
+          { body: 'Science' }
+        ]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ],
+  },
+  mutations: {
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards:[] })
+    },
+  },
+  actions: {
+    addlist(context, payload) {
+      context.commit('addlist', payload)
+    },
+  },
+  getters: {
+  }
+})
+
+store.subscribe((mutation, state) => {
+  localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+})
+
+export default store
