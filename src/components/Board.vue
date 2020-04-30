27467f04801b0e0468623640e49cd5fed2b71894
Konsei0516 <masaya19960516@gmail.com>
2020-04-30 13:19:19 +0900

    初期状態の削除

---
+++ src/components/Board.vue
@@ -0,0 +1,17 @@
+<template>
+　<!-- ここから追加 -->
+  <div>
+    <header>
+      my Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+    </main>
+  </div>
+  <!-- ここまで追加 -->
+</template>
+
+<script>
+export default {
+}
+</script>
