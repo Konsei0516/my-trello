870c5fb4ee6e4a017b6eef130ebf22954c01dc60
Konsei0516 <masaya19960516@gmail.com>
2020-05-05 17:39:03 +0900

    タスクのカウント
    カードのカウント
    全体タスクのカウントを可能にしました

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -4,7 +4,7 @@
       my Trello
     </header>
     <main>
-      <p class="info-line">All: 0 tasks</p>
+      <p class="info-line">All: {{ totalCardCount }}</p>
       <div class="list-index">
         <list v-for="(item, index) in lists"
               :key="item.id"
@@ -33,6 +33,9 @@ export default {
     ...mapState([
       'lists'
     ]),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount
+    }
   },
 }
 </script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -2,6 +2,7 @@
   <div class="list">
     <div class="listheader">
       <p class="list-title">{{ title }}</p>
+      <p class="list-counter">total: {{ totalCardInList }}</p>
       <div class="deletelist" @click="removeList">×</div>
     </div>
     <card v-for="(item, index) in cards"
@@ -34,6 +35,11 @@ export default {
     listIndex: {
       type: Number,
       required: true
+    }
+  },
+  computed: {
+    totalCardInList() {
+      return this.cards.length
     }
   },
   methods: {
